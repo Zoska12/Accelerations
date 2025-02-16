9741943351a8f98affe1697d131d9bc62ae5d387
Zofia Wróblewska <00TIM351@tim.pl>
2025-02-16 11:49:43 +0100

    Dodanie podziału treningu na części

--- generowanie_wynikow.py
+++ generowanie_wynikow.py
@@ -3,6 +3,13 @@ import re
 
 file_path = "15.01 Tr. 6 Export for Patryk Kusztal 45648.csv"
 
+# Definiowanie zakresu godzinowego
+start_time = "18:15"
+end_time = "18:30"
+
+podzial = {'Mała Gra':[start_time,end_time],
+           'Duża Gra':["18:30","18:45"]}
+
 # Pobranie imienia i nazwiska z sekcji # Athlete
 with open(file_path, 'r', encoding='utf-8') as file:
     lines = file.readlines()
@@ -32,38 +39,52 @@ def compute_future_mean(series, window=7):
 
 df['Acceleration_SMA'] = compute_future_mean(df['Acceleration'], window=7)
 
-# Definiowanie dynamicznych przedziałów co 5 jednostek
-max_velocity = df['Velocity'].max()  # Pobranie maksymalnej wartości
-bins = list(range(0, int(max_velocity) + 6, 5))  # Tworzenie przedziałów co 5
-labels = [f"{bins[i]}-{bins[i+1]}" for i in range(len(bins)-1)]  # Generowanie etykiet
+df['Timestamp'] = pd.to_datetime(df['Timestamp'], format="%d.%m.%Y %H:%M")
 
-# Przypisanie wartości do przedziałów
-df['Velocity_Bin'] = pd.cut(df['Velocity'], bins=bins, labels=labels, right=False)
+results=[]
+for key, value in podzial.items():
+    # Konwersja na obiekty czasu
+    df_filtred = df[(df['Timestamp'].dt.time >= pd.to_datetime(value[0], format='%H:%M').time()) & 
+                    (df['Timestamp'].dt.time <= pd.to_datetime(value[1], format='%H:%M').time())]
 
-# Znalezienie wszystkich wartości przyspieszenia dla każdej grupy z warunkiem minimalnej odległości 1s
-def filter_by_time_gap(group):
-    selected = []
-    top_n = 10  # Początkowa liczba wybranych wartości
-    while len(selected) < 10:
-        candidates = group.nlargest(top_n, 'Acceleration_SMA')  # Pobieramy dynamicznie rosnącą liczbę wyników
+    # Definiowanie dynamicznych przedziałów co 5 jednostek
+    max_velocity = df['Velocity'].max()  # Pobranie maksymalnej wartości
+    bins = [5, 10, 15]  # Dwa przedziały: 5-10 i 10-15
+    labels = ["5-10", "10-15"]
+
+    # Przypisanie wartości do przedziałów
+    df_filtred['Velocity_Bin'] = pd.cut(df_filtred['Velocity'], bins=bins, labels=labels, right=False)
+
+    df_filtred['Trening']=key
+    # Znalezienie wszystkich wartości przyspieszenia dla każdej grupy z warunkiem minimalnej odległości 1s
+    def filter_by_time_gap(group):
         selected = []
-        for _, row in candidates.iterrows():
-            if not selected or all(abs(row['Seconds'] - prev['Seconds']) >= 1 for prev in selected):
-                selected.append(row)
-            if len(selected) == 10:
+        top_n = 3  # Początkowa liczba wybranych wartości
+        while len(selected) < 3:
+            candidates = group.nlargest(top_n, 'Acceleration_SMA')  # Pobieramy dynamicznie rosnącą liczbę wyników
+            selected = []
+            for _, row in candidates.iterrows():
+                if not selected or all(abs(row['Seconds'] - prev['Seconds']) >= 1 for prev in selected):
+                    selected.append(row)
+                if len(selected) == 3:
+                    break
+            top_n += 3  # Zwiększamy liczbę wybieranych kandydatów, jeśli nie osiągnęliśmy 10 obserwacji
+            if top_n > len(group):  # Jeśli już nie mamy więcej danych, przerywamy pętlę
                 break
-        top_n += 5  # Zwiększamy liczbę wybieranych kandydatów, jeśli nie osiągnęliśmy 10 obserwacji
-        if top_n > len(group):  # Jeśli już nie mamy więcej danych, przerywamy pętlę
-            break
-    return pd.DataFrame(selected)
+        return pd.DataFrame(selected)
+    
 
-top_acceleration_by_bin = (
-    df.groupby('Velocity_Bin', group_keys=False)
-    .apply(filter_by_time_gap)
-    .sort_values(by=['Velocity_Bin', 'Seconds'])
-    .reset_index(drop=True)[['Timestamp', 'Seconds', 'Velocity', 'Acceleration_SMA', 'Velocity_Bin']]
-)
+    top_acceleration_by_bin = (
+        df_filtred.groupby('Velocity_Bin', group_keys=False)
+        .apply(filter_by_time_gap)
+        .sort_values(by=['Velocity_Bin', 'Seconds'])
+        .reset_index(drop=True)[['Timestamp', 'Seconds', 'Velocity', 'Acceleration_SMA', 'Velocity_Bin','Trening']]
+    )
+    results.append(top_acceleration_by_bin)
 # Zapis wyników do pliku
-top_acceleration_by_bin.to_excel(output_file, index=False)
+
+final_results = pd.concat(results, ignore_index=True)
+
+final_results.to_excel(output_file, index=False)
 
 print(f'Wyniki zapisane w pliku {output_file}')
--- main.py
+++ main.py
@@ -32,8 +32,7 @@ def extract_date(lines):
         return export_date
     return "Unknown_date"
 
-
-def process_data(file_path, lines):
+def process_file(file_path, lines):
 
     """ Przetwarza dane z pliku CSV, oblicza średnie przyspieszenie i przedziały prędkości. """
     header_line = next(i for i, line in enumerate(lines) if "Velocity" in line)
@@ -44,9 +43,27 @@ def process_data(file_path, lines):
 
     df['Acceleration_SMA'] = compute_future_mean(df['Acceleration'], window=7)
 
-    max_velocity = df['Velocity'].max()
-    #bins = list(range(0, int(max_velocity) + 6, 5))
-    #labels = [f"{bins[i]}-{bins[i+1]}" for i in range(len(bins)-1)]
+    return df
+
+def process_data(df, start_date=None,end_date=None):
+    # Zamiana pustych wartości na NaN, jeśli są
+    #df['Timestamp'] = df['Timestamp'].replace("", np.nan)
+    # Konwersja do datetime z obsługą błędów
+    #df['Timestamp'] = pd.to_datetime(df['Timestamp'], format="%d.%m.%Y %H:%M", errors='coerce')
+
+    df['Timestamp'] = pd.to_datetime(df['Timestamp'], errors='coerce', dayfirst=True)
+    # Konwersja Timestamp do HH:MM bez sekund
+    df['Timestamp_time'] = df['Timestamp'].dt.strftime('%H:%M')
+
+    if start_date is not None and end_date is not None:
+        # Konwersja start_date i end_date na ten sam format HH:MM (bez sekund)
+        start_time = pd.to_datetime(start_date, format='%H:%M').strftime('%H:%M')
+        end_time = pd.to_datetime(end_date, format='%H:%M').strftime('%H:%M')
+
+        # Filtrowanie danych na podstawie godzin
+        df = df[(df['Timestamp_time'] >= start_time) & (df['Timestamp_time'] <= end_time)]
+
+
     bins = [5, 10, 15]  # Dwa przedziały: 5-10 i 10-15
     labels = ["5-10", "10-15"]
     df['Velocity_Bin'] = pd.cut(df['Velocity'], bins=bins, labels=labels, right=False)
@@ -89,13 +106,21 @@ def process_data(file_path, lines):
 def save_to_excel(df, athlete_name, date, file_number, output_folder):
     """ Zapisuje DataFrame do pliku XLSX w podanym folderze. """
     os.makedirs(output_folder, exist_ok=True)  # Tworzy folder, jeśli nie istnieje
-    df['Timestamp'] = df['Timestamp'].fillna(date)
+    #print(type(df['Timestamp'].iloc[0]))
+    df['Timestamp'] = df['Timestamp'].fillna(pd.to_datetime(date, format='%d.%m.%Y'))
     output_file = os.path.join(output_folder, f'wyniki_{athlete_name}_{date}_{file_number}.xlsx')
     df.to_excel(output_file, index=False)
     print(f"Zapisano plik: {output_file}")
 
 # Pobranie ścieżki folderu od użytkownika
-folder_path = input("Podaj ścieżkę folderu z plikami CSV: ").strip()
+#folder_path = input("Podaj ścieżkę folderu z plikami CSV: ").strip()
+
+#TU WPISZ ŚCIEŻKĘ 
+folder_path = "Przykladowy Folder"
+folder_path = "Raw files GPS/Raw files GPS Zagłębie/czwartek"
+#folder_path = "Raw files GPS/Raw files GPS Zagłębie/środa"
+#folder_path = "Raw files GPS/Raw files GPS Zagłębie/wtorek"
+#folder_path = "Raw files GPS/Raw files GPS Zagłębie/poniedziałek"
 
 # Sprawdzenie, czy folder istnieje
 if not os.path.isdir(folder_path):
@@ -126,24 +151,53 @@ for file_number, file in enumerate(csv_files, start=1):
     athlete_name = extract_athlete_name(lines)
     date = extract_date(lines)
     # Przetwórz plik do DataFrame
-    df_processed = process_data(file_path, lines)
+    
+    df = process_file(file_path, lines)
+    
+    #TU PODAJ ZAKRES CZASU I TYP ĆWICZENIA DLADANEGO TRENINGU (folderu który przetwarzasz)
+    #podzial = {'Duża Gra':["14:10","14:15"], 'Mała Gra':["14:16","14:30"]}
 
-        # Grupowanie i obliczanie średnich
-    df_grouped = (
-        df_processed.groupby('Velocity_Bin')
+    #podzial = {'Duża Gra':["11:15","11:30"], 'Mała Gra':["11:31","12:00"]}
+    #JEŚLI WPISZESZ PUSTE NAWIASY [] TO WEŹMIE CAŁY PLIK  
+    podzial = {'Gra':[]}
+    results=[]
+
+    for key, value in podzial.items():
+
+
+        start_time = value[0] if len(value) > 0 and value[0] else None
+        end_time = value[1] if len(value) > 1 and value[1] else None
+
+        df_processed = process_data(df, start_time, end_time)
+
+        df_processed['Trening']=key
+            # Grupowanie i obliczanie średnich
+        df_grouped = (
+        df_processed.groupby(['Velocity_Bin', 'Trening'])
         .agg({'Acceleration_SMA': 'mean'})
         .reset_index()
-    )
+        )
 
+
+        results.append(df_processed)
+
+    final_results = pd.concat(results, ignore_index=True)    
     # Dodanie wyniku do listy
-    all_results.append(df_grouped)
+    all_results.append(final_results)
+
 
     # Zapisz wynik do Excela w folderze "wyniki"
-    save_to_excel(df_processed, athlete_name,date, file_number, output_folder)
+    save_to_excel(final_results, athlete_name,date, file_number, output_folder)
+ 
 
 # Łączenie wszystkich wyników w jeden DataFrame
 final_df = pd.concat(all_results, ignore_index=True)
-final_df=final_df.groupby('Velocity_Bin')['Acceleration_SMA'].mean().reset_index()
+
+if not final_df.empty:
+    final_df = final_df.groupby(['Velocity_Bin', 'Trening'], observed=False)['Acceleration_SMA'].mean().reset_index()
+else:
+    final_df = pd.DataFrame(columns=['Velocity_Bin', 'Trening', 'Acceleration_SMA'])
+
 # Wyświetlenie lub zapisanie wynikowego DataFrameS
 
 output_file = os.path.join(output_folder, f'wyniki_średnie.xlsx')
